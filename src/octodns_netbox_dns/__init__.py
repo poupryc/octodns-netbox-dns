60b322636169fd8a32804f29264fe750caff607b
Ivan Schaller <ivan@schaller.sh>
2024-06-27 21:26:56 +0200

    use make_absolute function

--- src/octodns_netbox_dns/__init__.py
+++ src/octodns_netbox_dns/__init__.py
@@ -438,4 +438,5 @@ class NetBoxDNSProvider(octodns.provider.base.BaseProvider):
         """
         query_params = {"status": "active", **self.nb_view}
         zones = self.api.plugins.netbox_dns.zones.filter(**query_params)
-        return sorted([z.name + ("." if z.name[-1] != "." else "") for z in zones])
+        
+        return sorted([self._make_absolute(z.name) for z in zones])
